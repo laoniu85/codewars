99de8bd41f9407642c6eb1e2e17be9fa2f1a1764
YongliuLi <ylli@thoughtworks.com>
2016-11-05 14:37:52 +0800

    [#0] longest consec

---
+++ src/main/java/com/laoniu/codewars/LongestConsec.java
@@ -0,0 +1,36 @@
+package com.laoniu.codewars;
+
+import java.util.Arrays;
+
+class LongestConsec {
+
+    public static int lengthConsec(String[] strarr, int start, int k) {
+        int sum = 0;
+        for (int i = 0; i < k && (start + i < strarr.length); i++) {
+            sum += strarr[start + i].length();
+        }
+        return sum;
+    }
+
+    public static int longest(String[] strarr, int k) {
+        int maxLen = 0;
+        int maxLenIdx = -1;
+        for (int i = 0; i < strarr.length; i++) {
+            int len = lengthConsec(strarr, i, k);
+            if (len > maxLen) {
+                maxLenIdx = i;
+                maxLen = len;
+            }
+        }
+        return maxLenIdx;
+    }
+
+    public static String longestConsec(String[] strarr, int k) {
+        // your code
+        if (strarr.length == 0 || k > strarr.length || k <= 0) {
+            return "";
+        }
+        int longest = longest(strarr, k);
+        return String.join("", Arrays.asList(strarr).subList(longest, longest + k));
+    }
+}
